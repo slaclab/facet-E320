40160ab0344a3c8dd6a9fa1169cfae33e9e94df4
Stephan Kuschel <stephan.kuschel@gmail.com>
2022-03-21 16:07:13 +0100

    add versioneer

--- E320/__init__.py
+++ E320/__init__.py
@@ -1,1 +1,6 @@
 # This file is part of E320
+
+from . import _version
+__version__ = _version.get_versions()['version']
+
+print('This is E320 version {}'.format(__version__))
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 from setuptools import setup, find_packages
+import versioneer
 
 
 setup(name='E320',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       packages=find_packages(include=['E320*']))
